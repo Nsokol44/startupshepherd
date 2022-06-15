dbd0c6192babaa2be50c787fef9753d25052cffa
Nicholas Sokol <n44@nicholass-mini.lan>
2022-06-15 12:18:28 -0400

    v0.3.1

--- src/views/Donate.vue
+++ src/views/Donate.vue
@@ -1,15 +1,31 @@
 <template>
   <div>
-    <stripe-checkout 
-      ref="checkoutRef"
-      mode="payment"
-      :pk="publishableKey"
-      :line-items="lineItems"
-      :success-url="successURL"
-      :cancel-url="cancelURL"
-      @loading="v => loading = v"
-    />
-    <v-btn @click="submit">Pay now!</v-btn>
+    <section>
+      <v-layout column wrap class="my-5" align-center>
+        <v-flex xs12 sm4 class="my-2 pa-2">
+          <h1>Every Contribution Helps the Site Run</h1>
+        </v-flex>
+
+        <v-flex xs12 sm4 class="my-2 pa-2">
+          <v-card class="text-center">
+            <v-img
+              height="350"
+               :src="require('../assets/venmo_qr.png')"
+            ></v-img>
+            <stripe-checkout
+              ref="checkoutRef"
+              mode="payment"
+              :pk="publishableKey"
+              :line-items="lineItems"
+              :success-url="successURL"
+              :cancel-url="cancelURL"
+              @loading="(v) => (loading = v)"
+            />
+            <v-btn @click="submit">Donate Today!</v-btn>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </section>
   </div>
 </template>
 
@@ -17,12 +33,13 @@
 import StripeCheckout from "@vue-stripe/vue-stripe";
 
 export default {
-    name: "donate",
+  name: "donate",
   components: {
     StripeCheckout,
   },
   data() {
-    this.publishableKey = process.env.pk_live_51L9sOsGG9hsxbmLftJ0KKSt091NiFrWOsK3H7ozF1X3J7eqqSW2WT078XvUG2spS3jVpDh9XXc6Z1sM28ydQfb7D00IBMSo7o7;
+    this.publishableKey =
+      process.env.pk_live_51L9sOsGG9hsxbmLftJ0KKSt091NiFrWOsK3H7ozF1X3J7eqqSW2WT078XvUG2spS3jVpDh9XXc6Z1sM28ydQfb7D00IBMSo7o7;
     return {
       loading: false,
       lineItems: [
